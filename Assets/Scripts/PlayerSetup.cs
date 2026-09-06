170cf59970a6499ab919980e8a478c94b55f392a
DLamilla20 <lamilla.tenazoa@gmail.com>
2026-09-05 20:23:19 -0500

    Update Player Animation

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -5,14 +5,21 @@ public class PlayerSetup : MonoBehaviourPun
 {
     [SerializeField] private Camera playerCamera;
     [SerializeField] private AudioListener audioListener;
+    [SerializeField] private SkinnedMeshRenderer[] renderersToDisable;
 
     void Start()
     {
-        Debug.Log($"IsMine: {photonView.IsMine} - {gameObject.name}");
         if (!photonView.IsMine)
         {
             playerCamera.gameObject.SetActive(false);
             if (audioListener != null) audioListener.enabled = false;
+        }
+        else
+        {
+            foreach (var renderer in renderersToDisable)
+            {
+                renderer.enabled = false;
+            }
         }
     }
 }
